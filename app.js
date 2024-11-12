9ab2f5cbe98a1382516225cb34a095f4586f1e35
Kauane Melis Schuelter <147657430+kauschuelter12@users.noreply.github.com>
2024-11-12 14:35:04 -0300

    .

--- app.js
+++ app.js
@@ -4,3 +4,14 @@ function criaCartao (categoria, pergunta, resposta) {
     cartao.className = 'cartao'
 
 }
+cartao.innerHTML = `
+        <div class="cartao__conteudo">
+        <h3>${categoria}</h3>
+        <div class="cartao__conteudo__pergunta">
+                <p>${pergunta}</p>
+        </div>
+        <div class="cartao__conteudo__resposta">
+                <p>${resposta}</p>
+        </div>
+        </div>
+`
