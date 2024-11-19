eb42228d1225ca6f6119339372a577f35adeef33
Kauane Melis Schuelter <147657430+kauschuelter12@users.noreply.github.com>
2024-11-19 14:35:46 -0300

    ,

--- perguntas.js
+++ perguntas.js
@@ -1,34 +1,35 @@
-'Programação',
-'O que é Python?',
-'O Python é uma linguagem de programação'
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
 )
 
 criaCartao(
-'Geografia',
-'Qual a capital da França?',
-'A capital da França é Paris'
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
 )
 
 criaCartao(
-'Programação',
-'O que é uma função?',
-'Uma função é um bloco de código que executa alguma tarefa'
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
 )
 
 criaCartao(
-'Lingua inglesa',
-'Como se diz oi em Inglês?',
-'Oi em ingles é HI (RAI)'
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
 )
 
 criaCartao(
-'cores',
-'qual é a cor do céu ?',
-'é azul'
+    'cores',
+    'qual é a cor do céu ?',
+    'é azul'
 )
 
 criaCartao(
-'Matematica',
-'Quantos é 2+2?',
-'a reposta é 4'
+    'Matematica',
+    'Quantos é 2+2?',
+    'a reposta é 4'
 )
